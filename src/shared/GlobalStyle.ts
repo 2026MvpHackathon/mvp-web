b7c798993b0a29ccd29921f13e31bb173ffe5ce6
이윤지 <yoonji0879@dgsw.hs.kr>
2026-02-05 17:59:57 +0900

    feat: add design tokens and theme

---
+++ src/shared/GlobalStyle.ts
@@ -0,0 +1,69 @@
+import { createGlobalStyle } from 'styled-components';
+import * as token from "./values/token"
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    font-family: ${token.fontFamily}, sans-serif;
+    color: ${token.colors.text.normal};
+    background-color: ${token.colors.background.Dark};
+    line-height: 1.5;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    /* 드래그 방지 */
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-user-drag: none;
+  }
+
+  img {
+    /* 드래그 방지 */
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-user-drag: none;
+  }
+
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  ul, ol {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    font-family: inherit;
+  }
+
+  input, textarea, select {
+    font-family: inherit;
+    /* 입력창은 드래그 가능해야 함 */
+    -webkit-user-select: text;
+    user-select: text;
+  }
+`;
---
+++ src/shared/values/_foundation.ts
@@ -0,0 +1,60 @@
+/**
+ *
+ * 예시:
+ *   color: ${colors.text.black};
+ *   background-color: ${colors.background.f5};
+ */
+
+export const colors = {
+  main: {
+    normal: "#E7EFDA",
+    alternative: "#0D2D1E",
+    assistive: "#3D5E3F",
+  },
+
+  secondary: {
+    normal: "#3D5F3E",
+    alternative: "#779B7F",
+    assistive: "#B7C9B9",
+  },
+
+  text: {
+    normal: "#F7F5F2",
+    strong: "#70D586",
+    neutral: "#7D8A80",
+    alternative: "#899F8B",
+    alternative2: "#616161",
+    disabled: "#D1D1D1",
+  },
+
+  line: {
+    normal: "#646764",
+    neutral: "#3D5F3E",
+    alternative: "#104912",
+    alternative2: "#464545",
+    alternative3: "#202020",
+  },
+
+  fill: {
+    normal: "#ACACAC",
+    neutral: "#7D8A80",
+    alternative: "#348B76",
+    assistive: "#2C362E",
+    alternative2: "#303030",
+    alternative3: "#131A13",
+    alternative4: "#151515",
+  },
+
+  background: {
+    Dark: "#080808",
+  },
+
+  state: {
+    error: "#CC2121",
+    info: "#1A74DD",
+    success: "#27D272",
+    warning: "#D4A42E",
+  },
+} as const;
+
+export type colors = typeof colors;
