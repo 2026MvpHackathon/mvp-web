750adf645256ddab2f472ff64e76c203996cd263
이윤지 <yoonji0879@dgsw.hs.kr>
2026-02-09 03:40:37 +0900

    chore: Remove comments

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-// HomePage.tsx
 import Recent from "@/widgets/home/recent_item/ui/Recent";
 import ObjectItem from "@/widgets/home/object_item/Object";
 import Search from "@/widgets/home/search-bar/SearchBar";
@@ -10,7 +9,6 @@ const HomePage = () => {
   return (
     <>
       <Recent />
-      {/* SearchBar에 검색 완료 시 setKeyword 호출 */}
       <Search onSearch={setKeyword} />
       <ObjectItem keyword={keyword} />
     </>
