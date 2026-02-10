8a07f9b9e6740aa14bd9c187ec4cc4d084cb4c09
민선영 <ttt4306@gmail.com>
2026-02-10 12:36:38 +0900

    feat: 퀴즈 마지막 flow

--- src/shared/ui/Layout/Layout.style.ts
+++ src/shared/ui/Layout/Layout.style.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
 import { flexColumnCenter } from '@/shared/values/_flex'
+import { typography } from '@/shared/values/typography.mixin'
+import { colors } from '@/shared/values/_foundation'
 
 export const container = styled.div`
     ${flexColumnCenter};
     width: 100vw;
     height: 100vh;
+    position: relative;
 `
 
 export const body = styled.div`
@@ -14,3 +17,47 @@ export const body = styled.div`
     padding: 2.25rem 2.5rem;
 `
 
+// blur
+
+export const blur_overlay = styled.div`
+  position: fixed;
+  inset: 0;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  backdrop-filter: blur(8px);
+  background: rgba(0, 0, 0, 0.25);
+
+  z-index: 100;
+  pointer-events: none; /* 아래 클릭 허용 */
+`
+
+export const top_ui = styled.div`
+  position: fixed;
+  inset: 0;
+  z-index: 200;
+
+  ${flexColumnCenter};
+  gap: 2.25rem;
+  width: 100%; // 너비를 100%로 변경
+`
+
+export const text_wrapper = styled.div`
+    width: 100%;
+    ${flexColumnCenter};
+`
+
+export const accuracy_rate = styled.span`
+    ${typography("heading","xxl","bold")};
+    color: ${colors.text.strong};
+`
+
+export const evaluation = styled.span`
+    ${typography("heading","lg","semibold")};
+    color: ${colors.text.normal};
+    white-space: pre;
+`
